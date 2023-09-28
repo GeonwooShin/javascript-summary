3195cb43b37df5e2bf5a0b9b777517f665d48855
GeonwooShin <rjsdnslawkd@naver.com>
2023-09-28 16:09:27 +0900

    test: test 작성 테스트

---
+++ __tests__/jestTest.test.js
@@ -0,0 +1,10 @@
+const sum = require("../jestTest");
+
+describe("첫 테스트", () => {
+  it("a와 b 더하기", () => {
+    let result = sum(1, 2);
+    expect(result).toBe(3);
+    result = sum(3, 4);
+    expect(result).toBe(7);
+  });
+});
