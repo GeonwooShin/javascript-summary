84da8d0e6ef541eeaf3765bf19e857478dfe2449
GeonwooShin <rjsdnslawkd@naver.com>
2023-09-28 16:09:03 +0900

    test: deepclone 테스트 작성

---
+++ __tests__/deepclone.test.js
@@ -0,0 +1,23 @@
+const { deepClone } = require("../deepclone");
+
+test("deeply copies objects", () => {
+  const obj = {
+    num: 1,
+    str: "string",
+    arr: [1, 2, 3, { inner: "obj" }],
+    obj: {
+      key: "value",
+    },
+    date: new Date(),
+    regex: /hello/g,
+  };
+
+  const copied = deepClone(obj);
+  expect(copied).toEqual(obj); // 값은 동일해야 합니다.
+  expect(copied).not.toBe(obj); // 하지만 참조는 다르게 됩니다.
+  expect(copied.arr).not.toBe(obj.arr);
+  expect(copied.arr[3]).not.toBe(obj.arr[3]);
+  expect(copied.obj).not.toBe(obj.obj);
+  expect(copied.date).not.toBe(obj.date);
+  expect(copied.regex).not.toBe(obj.regex);
+});
